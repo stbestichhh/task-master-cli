0009bb7906e691dd1bdb95ad5bbfabe2c9aa7d5d
Kiril Yakymchuk <stbestich@gmail.com>
2024-02-12 14:25:34 +0200

    Connected to database

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const mongodb_pass = process.env.MONGODB_PASS;
+
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connect(
+  `mongodb+srv://stbestichhh:${mongodb_pass}@cluster0.i1n9ohq.mongodb.net/`
+);
+
+module.exports = db;
