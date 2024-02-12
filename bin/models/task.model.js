4d618784fb64590df197968feebb8ee333e45555
Kiril Yakymchuk <stbestich@gmail.com>
2024-02-12 14:28:56 +0200

    Updated task schema

--- bin/models/task.model.js
+++ bin/models/task.model.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const taskSchema = mongoose.Schema({
-  title: { type: String },
-  description: { type: String },
-  deadline: { type: String },
-  status: { type: String },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  deadline: { type: String, required: true },
+  status: { type: String, enum: ['done', 'pending'], default: 'pending' },
 });
 
 module.exports = mongoose.model('Task', taskSchema);
