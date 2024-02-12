984f1aaeb20d8170c8e83ff7961f833a2bb3c81c
Kiril Yakymchuk <stbestich@gmail.com>
2024-02-12 13:04:06 +0200

    List tasks

--- bin/commands/cli.js
+++ bin/commands/cli.js
@@ -1,26 +1,34 @@
 const yargs = require('yargs');
 
-yargs.command({
-  command: 'add',
-  describe: 'Add new task',
-  builder: {
-    title: {
-      describe: 'Task title',
-      demandOption: true,
-      type: 'string',
+yargs
+  .command({
+    command: 'add',
+    describe: 'Add new task',
+    builder: {
+      title: {
+        describe: 'Task title',
+        demandOption: true,
+        type: 'string',
+      },
+      description: {
+        describe: 'Task description',
+        demandOption: true,
+        type: 'string',
+      },
+      deadLine: {
+        describe: 'Task deadline (DD-MM-YYYY)',
+        demandOption: true,
+        type: 'string',
+      },
     },
-    description: {
-      describe: 'Task description',
-      demandOption: true,
-      type: 'string',
+    handler: (argv) => {
+      //addTask
     },
-    deadLine: {
-      describe: 'Task deadline (DD-MM-YYYY)',
-      demandOption: true,
-      type: 'string',
+  })
+  .command({
+    command: ['list', 'ls'],
+    describe: 'Show list of all the tasks',
+    handler: () => {
+      //listTasks
     },
-  },
-  handler: (argv) => {
-    //addTask
-  }
-});
+  });
