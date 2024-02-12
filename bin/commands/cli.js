0c7c2d7a11db74afefeb78451f0a8eea5b343321
Kiril Yakymchuk <stbestich@gmail.com>
2024-02-12 13:34:57 +0200

    Update command

--- bin/commands/cli.js
+++ bin/commands/cli.js
@@ -6,25 +6,27 @@ yargs
     command: 'add',
     describe: 'Add new task',
     handler: (argv) => {
-      inquirer.promt([
-        {
-          type: 'input',
-          name: 'Title',
-          message: 'Enter task name:',
-        },
-        {
-          type: 'input',
-          name: 'Description',
-          message: 'Write more about task:',
-        },
-        {
-          type: 'input',
-          name: 'Deadline',
-          message: 'Enter task deadline (DD-MM-YYYY)',
-        },
-      ]).then((answers) => {
-        //addTask 
-      });
+      inquirer
+        .promt([
+          {
+            type: 'input',
+            name: 'Title',
+            message: 'Enter task name:',
+          },
+          {
+            type: 'input',
+            name: 'Description',
+            message: 'Write more about task:',
+          },
+          {
+            type: 'input',
+            name: 'Deadline',
+            message: 'Enter task deadline (DD-MM-YYYY):',
+          },
+        ])
+        .then((answers) => {
+          //addTask
+        });
     },
   })
   .command({
@@ -32,5 +34,58 @@ yargs
     describe: 'Show list of all the tasks',
     handler: () => {
       //listTasks
+    },
+  })
+  .command({
+    command: ['update', 'u'],
+    describe: 'Update task properties',
+    handler: () => {
+      inquirer
+        .promt([
+          {
+            type: 'input',
+            name: 'name',
+            message: 'Enter task name to update:',
+          },
+          {
+            type: 'checkbox',
+            name: 'properties',
+            message: 'Select properties to update:',
+            choices: [
+              { name: 'Task name' },
+              { name: 'Description' },
+              { name: 'Deadline' },
+              { name: 'Status' },
+            ],
+          },
+          {
+            type: 'input',
+            name: 'newTitle',
+            message: 'Enter new title:',
+            when: (answers) => answers.properties.includes('Title'),
+          },
+          {
+            type: 'input',
+            name: 'newDescription',
+            message: 'Enter new description:',
+            when: (answers) => answers.properties.includes('Description'),
+          },
+          {
+            type: 'input',
+            name: 'newDeadline',
+            message: 'Enter new deadline (YYYY-MM-DD):',
+            when: (answers) => answers.properties.includes('Deadline'),
+          },
+          {
+            type: 'list',
+            name: 'newStatus',
+            message: 'Select new status:',
+            choices: ['done', 'pending'],
+            when: (answers) => answers.properties.includes('Status'),
+          },
+        ])
+        .then((answers) => {
+          //updateTask
+        });
     },
   });
