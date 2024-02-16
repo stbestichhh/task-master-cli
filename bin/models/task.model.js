ddbfbb7b139573be7b50581d62430063ce5c7b16
Kiril Yakymchuk <stbestich@gmail.com>
2024-02-16 18:12:20 +0200

    Task save

--- bin/models/task.model.js
+++ bin/models/task.model.js
@@ -7,4 +7,15 @@ class Task {
     this.deadline = deadline;
     this.status = status;
   }
+
+  save() {
+    const q = 'INSERT INTO tasks (title, description, deadline, status) VALUES (?, ?, ?, ?)';
+    const values = [this.title, this.description, this.deadline, this.status];
+    db.run(q, values, (err) => {
+      if (err) {
+        return console.log(err.message);
+      }
+      return console.log('Task has been added: ', this);
+    });
+  }
 }
