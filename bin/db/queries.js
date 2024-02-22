3a90d1ddb8dfff05441ad9e5df5e8ebd05953905
Kiril Yakymchuk <stbestich@gmail.com>
2024-02-22 13:39:27 +0200

    Created db queries obj

---
+++ bin/db/queries.js
@@ -0,0 +1,16 @@
+const queries = {
+  execute: `CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    deadline DATE NOT NULL,
+    status TEXT NOT NULL DEFAULT 'pending'
+  )`,
+  getAll: 'SELECT * FROM tasks',
+  getOne: 'SELECT * FROM tasks WHERE title = ?',
+  add: 'INSERT INTO tasks (title, description, deadline, status) VALUES (?, ?, ?, ?)',
+  update: 'UPDATE tasks SET title = ?, description = ?, deadline = ?, status = ? WHERE title = ?',
+  delete: 'DELETE FROM tasks WHERE title = ?'
+};
+
+module.exports = queries;
