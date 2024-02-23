6a2f0314fb3698061bc651da3ebdd3983df0326f
Kiril Yakymchuk <stbestich@gmail.com>
2024-02-23 19:25:11 +0200

    Fixed bug with db

--- bin/db/queries.js
+++ bin/db/queries.js
@@ -1,11 +1,4 @@
 const queries = {
-  execute: `CREATE TABLE IF NOT EXISTS tasks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL,
-    deadline DATE NOT NULL,
-    status TEXT NOT NULL DEFAULT 'pending'
-  )`,
   getAll: 'SELECT * FROM tasks',
   getOne: 'SELECT * FROM tasks WHERE title = ?',
   add: 'INSERT INTO tasks (title, description, deadline, status) VALUES (?, ?, ?, ?)',
